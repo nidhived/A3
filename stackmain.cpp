0d089db5cab1c802507a2614176303ae608b4f87
Nidhi Vedantam <nidhived@Nidhis-MBP.attlocal.net>
2020-03-31 11:47:27 -0700

    main in progress

---
+++ stackmain.cpp
@@ -0,0 +1,31 @@
+//#include "genstack.cpp"
+#include "delim.h"
+
+using namespace std;
+
+int main(int argc, char **argv)
+{
+
+    delim objecttest;
+    cout << objecttest.openfile("qmain.cpp");
+//   GenStack *myStack = new GenStack(10);
+//
+//   myStack->push('R');
+//   myStack->push('e');
+//   myStack->push('n');
+//   myStack->push('e');
+//
+//   cout << "popping: " << myStack->pop() << endl;
+//   cout << "peek-a-boo: " << myStack->peek() << endl;
+//
+//   myStack->push('F');
+//   cout << "peek-a-boo: " << myStack->peek() << endl;
+//
+//   while (!myStack->isEmpty()){
+//   cout << "popping: " << myStack->pop() << endl;
+// }
+//   cout << "is stack empty? " << myStack->isEmpty() << endl;
+//
+//   delete myStack;
+//   return 0;
+}
